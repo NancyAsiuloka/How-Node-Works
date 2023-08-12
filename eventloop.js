33eaeb36f1d9436b8afb2ad2a49b1310724340b6
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-12 06:12:43 +0100

    added a callback function

--- eventloop.js
+++ eventloop.js
@@ -3,4 +3,6 @@ const fs = required('fs');
 setTimeout(() => console.log('Timer 1 is finished'), 0);
 setImmediate(() => console.log('Immediate 1 finished'));
 
-fs.readFile('text-file.txt')
+fs.readFile('text-file.txt', () => {
+    console.log('i/o finished');
+});
