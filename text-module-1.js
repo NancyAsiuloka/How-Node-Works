5f8bd9ba09e91473e91ae0688b8363c2b5f04f95
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-14 18:44:24 +0100

    added a comment

--- text-module-1.js
+++ text-module-1.js
@@ -20,7 +20,7 @@ module.exports = class {
         add(a, b) {
             return a + b;
         }
-        subtract(a, b) {
+        subtract (a, b) {
             return a - b;
         }
         multiply(a, b) {
